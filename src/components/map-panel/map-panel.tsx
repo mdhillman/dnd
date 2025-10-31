7b4b3ad75ee70ec8588aaaa398abd2f7939f8bd8
Michael Hillman <michael.hillman@gearset.com>
2025-10-31 19:10:54 +0000

    Add material UI

--- src/components/map-panel/map-panel.tsx
+++ src/components/map-panel/map-panel.tsx
@@ -4,7 +4,9 @@ import {Map} from "mapbox-gl";
 
 import 'mapbox-gl/dist/mapbox-gl.css';
 import styles from './map-panel.module.css';
-
+import { IconButton} from "@mui/material";
+import MapIcon from '@mui/icons-material/Map';
+import PublicIcon from '@mui/icons-material/Public';
 
 function MapPanel() {
     const mapRef = useRef<Map | null>(null)
@@ -14,6 +16,7 @@ function MapPanel() {
         mapboxgl.accessToken = 'pk.eyJ1IjoibWhpbGxtYW4iLCJhIjoiY21oYjB3c3J3MDA3ZTJvcXYyam9pZmtpYyJ9.E7J0teWhy4JAwZWI4K7G_w'
         mapRef.current = new mapboxgl.Map({
             container: mapContainerRef.current ?? '',
+            style: 'mapbox://styles/mhillman/cmhf3gk7s007901qx4viw0q70',
             center: [-74.0060152, 40.7127281],
             zoom: 5,
         });
@@ -23,9 +26,26 @@ function MapPanel() {
         }
     }, [])
 
+    const changeProjection = (projection: string) => {
+        if(mapRef.current) {
+            mapRef.current.setProjection(projection);
+        }
+    }
+
     return (
-        <div id='map' className={styles.wrapper} ref={mapContainerRef}>
-        </div>
+        <>
+            <div id='map' className={styles.wrapper} ref={mapContainerRef}></div>
+            <div className={styles.projectionContainer}>
+                <IconButton aria-label="mercator-projection" onClick={() => changeProjection('mercator')}>
+                    <MapIcon className={styles.projectionIcon}/>
+                </IconButton>
+                <IconButton aria-label="globe-projection" onClick={() => changeProjection('globe')}>
+                    <PublicIcon />
+                </IconButton>
+            </div> 
+        </>
+       
+
     );
 }
 
