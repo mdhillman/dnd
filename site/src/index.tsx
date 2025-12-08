84086cac9f47ac11379683c0e84576d54f6fc0f9
Michael Hillman <michael.hillman@gearset.com>
2025-12-08 18:11:42 +0000

    oughqaegpgiujhghpisr[joh

--- site/src/index.tsx
+++ site/src/index.tsx
@@ -2,35 +2,39 @@ import { FC } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import MapPanel from './components/map-panel/map-panel';
-import { useSearchParams } from "react-router";
+import { useNavigate, useSearchParams } from "react-router";
 import PageWrapper from './components/page-wrapper/page-wrapper';
 import InfoPanel from './components/info-panel/info-panel';
+import { Buffer } from 'buffer';
 
 import './global.css';
+import { LandingPage } from './components/landing-page/landing-page';
+import { getCodesFromCookie } from './utilties';
+
+
+
 
 // Check if the Buffer global is defined, if not, attach the polyfill
-import { Buffer } from 'buffer';
-import { HomePage } from './components/home-page/home-page';
 if (typeof window !== 'undefined' && typeof window.Buffer === 'undefined') {
   window.Buffer = Buffer;
 }
 
 // Main page
 const MainPage: FC = () => {
-    let [params] = useSearchParams();
-    const infoParam = params.get("info");
+    //let [params] = useSearchParams();
+    //const infoParam = params.get("info");
 
-    if(infoParam) {
-        return (
-            <PageWrapper>
-                <InfoPanel filename={infoParam}/>
-            </PageWrapper>
-        )
-    } 
+    const navigate = useNavigate();
     
-    return (
-        <PageWrapper/>
-    )
+    const cookies = getCodesFromCookie();
+    if(!cookies.includes('accept-cookies')) {
+        navigate('/landing');
+        return;
+    }
+
+    <PageWrapper>
+        <InfoPanel/>
+    </PageWrapper>
 }
 
 /**
@@ -40,8 +44,9 @@ const MainPage: FC = () => {
 const CustomRoutes: FC = () => (
     <Routes>
         <Route path='/' element={<MainPage/>}/>
-        <Route path='/landing' element={<HomePage/>}/>
+        <Route path='/landing' element={<LandingPage/>}/>
         <Route path='/map' element={<MapPanel/>}/>
+        <Route path='/info' element={<InfoPanel filename='home'/>}/>
     </Routes>
 );
 
